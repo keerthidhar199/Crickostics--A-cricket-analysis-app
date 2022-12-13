2aca33296e8092552893fddfbed3384a58df6528
keerthidhar199 <keerthidhar1999@gmail.com>
2022-12-14 00:59:45 +0530

    Setting Color palette

--- lib/views/fantasy_players_UI.dart
+++ lib/views/fantasy_players_UI.dart
@@ -50,13 +50,14 @@ class _fantasyteamState extends State<fantasyteam> {
 
   @override
   Widget build(BuildContext context) {
-    // var fantasyiterator = fantasydata.iterator;
-    // while (fantasyiterator.moveNext()) {
-    //   setState(() {
-    //     distinct_leagues.add(
-    //         fantasyiterator.current['league_team'][0].toString().split('_')[0]);
-    //   });
-    // }
+    var fantasyiterator = fantasydata.keys.iterator;
+    while (fantasyiterator.moveNext()) {
+      setState(() {
+        distinct_leagues
+            .add(fantasyiterator.current.toString().split('_').first);
+      });
+    }
+    print('object $distinct_leagues');
     randomColors = randomColors..shuffle();
     var Color1;
 
@@ -125,10 +126,24 @@ class _fantasyteamState extends State<fantasyteam> {
                 ? GroupedListView<dynamic, String>(
                     elements: fantasydata.keys.toList(),
                     groupBy: (element) => element.toString().split('_').first,
-                    groupSeparatorBuilder: (String groupByValue) {
+                    groupHeaderBuilder: (e) {
+                      Color1 = randomColors[distinct_leagues
+                          .toList()
+                          .indexOf(e.toString().split('_').first)];
                       return Container(
+                        // decoration: BoxDecoration(
+                        //     borderRadius: BorderRadius.circular(20.0),
+                        //     gradient: LinearGradient(
+                        //       begin: Alignment.bottomLeft,
+                        //       end: Alignment.topRight,
+                        //       colors: [
+                        //         Color1,
+                        //         Color1.withOpacity(0.85),
+
+                        //       ],
+                        //     )),
                         child: Text(
-                          groupByValue,
+                          e.toString().split('_').first,
                           style: globals.noble,
                           textScaleFactor: 1.255,
                           textAlign: TextAlign.center,
@@ -136,8 +151,9 @@ class _fantasyteamState extends State<fantasyteam> {
                       );
                     },
                     indexedItemBuilder: (context, e, index) {
-                      Color1 =
-                          randomColors[fantasydata.keys.toList().indexOf(e)];
+                      Color1 = randomColors[distinct_leagues
+                          .toList()
+                          .indexOf(e.toString().split('_').first)];
                       List teamsplit = e.toString().split('_')[1].split('vs');
                       String vs = teamsplit[0] + ' vs ' + teamsplit[1];
                       return Column(children: [
@@ -150,8 +166,6 @@ class _fantasyteamState extends State<fantasyteam> {
                                   colors: [
                                     Color1,
                                     Color1.withOpacity(0.85),
-                                    // Color(0xff5983F3),
-                                    // Color(0xff5983F3).withOpacity(0.6),
                                   ],
                                 )),
                             child: ExpansionTile(
@@ -231,260 +245,6 @@ class _fantasyteamState extends State<fantasyteam> {
                       ]);
                       // return Text(element.toString());
                     })
-                //  SingleChildScrollView(
-                //     child: Column(
-                //         children: fantasydata.keys.map((e) {
-                //       Color1 =
-                //           randomColors[fantasydata.keys.toList().indexOf(e)];
-                //       String league_name = e.toString().split('_').first;
-                //       List teamsplit = e.toString().split('_')[1].split('vs');
-                //       String vs = teamsplit[0] + ' vs ' + teamsplit[1];
-
-                //       return Column(
-                //         children: [
-                //           Column(
-                //             children: [
-                //               Text(
-                //                 league_name,
-                //                 style: globals.noble,
-                //                 textScaleFactor: 1.255,
-                //                 textAlign: TextAlign.center,
-                //               ),
-                //               Container(
-                //                 decoration: BoxDecoration(
-                //                     borderRadius: BorderRadius.circular(20.0),
-                //                     gradient: LinearGradient(
-                //                       begin: Alignment.bottomLeft,
-                //                       end: Alignment.topRight,
-                //                       colors: [
-                //                         Color1,
-                //                         Color1.withOpacity(0.85),
-                //                         // Color(0xff5983F3),
-                //                         // Color(0xff5983F3).withOpacity(0.6),
-                //                       ],
-                //                     )),
-                //                 child: ExpansionTile(
-                //                   trailing: Icon(
-                //                     Icons.arrow_drop_down,
-                //                     size: 25,
-                //                   ),
-                //                   title: Text(
-                //                     vs,
-                //                     style: globals.noble,
-                //                   ),
-                //                   children: fantasydata[e].map((eachteam) {
-                //                     return Column(
-                //                       children: [
-                //                         Text(
-                //                           teamsplit[fantasydata[e]
-                //                               .toList()
-                //                               .indexOf(eachteam)],
-                //                           style: globals.noble,
-                //                         ),
-                //                         Container(
-                //                           width:
-                //                               MediaQuery.of(context).size.width,
-                //                           child: Card(
-                //                             shape: RoundedRectangleBorder(
-                //                               borderRadius:
-                //                                   BorderRadius.circular(20.0),
-                //                             ),
-                //                             child: Container(
-                //                               decoration: BoxDecoration(
-                //                                   borderRadius:
-                //                                       BorderRadius.circular(
-                //                                           20.0),
-                //                                   gradient: LinearGradient(
-                //                                     begin: Alignment.bottomLeft,
-                //                                     end: Alignment.topRight,
-                //                                     colors: [
-                //                                       Color1.withOpacity(0.55),
-                //                                       Color1.withOpacity(0.8),
-
-                //                                       // Color(0xff5983F3),
-                //                                       // Color(0xff5983F3).withOpacity(0.6),
-                //                                     ],
-                //                                   )),
-                //                               padding: const EdgeInsets.all(8),
-                //                               child: Column(
-                //                                 children: [
-                //                                   Text(
-                //                                     'Your Selection',
-                //                                     style: globals.noble,
-                //                                   ),
-                //                                   for (var as in eachteam.keys)
-                //                                     Column(
-                //                                       children: [
-                //                                         Text(
-                //                                           as.toString(),
-                //                                           style: globals.noble,
-                //                                         ),
-                //                                         for (var player
-                //                                             in eachteam[as])
-                //                                           Column(
-                //                                             children: [
-                //                                               Text(player
-                //                                                   .toString()),
-                //                                             ],
-                //                                           )
-                //                                       ],
-                //                                     ),
-                //                                 ],
-                //                               ),
-                //                             ),
-                //                           ),
-                //                         ),
-                //                       ],
-                //                     );
-                //                   }).toList(),
-                //                 ),
-                //               )
-                //             ],
-                //           ),
-                //         ],
-                //       );
-                //     }).toList()),
-                //   )
-
-                // GroupedListView<Map<String, List<dynamic>>, String>(
-                //     elements: fantasydata,
-                //     groupBy: (element) => element.keys.toList().first,
-                //     groupSeparatorBuilder: (String groupByValue) {
-                //       return Column(
-                //         children: [
-                //           Text(
-                //             groupByValue.split('_')[0],
-                //             style: globals.noble,
-                //             textAlign: TextAlign.center,
-                //           ),
-                //           Text(
-                //             groupByValue.split('_')[1].split('vs').first +
-                //                 ' vs ' +
-                //                 groupByValue.split('_')[1].split('vs').last,
-                //             style: globals.noble,
-                //             textAlign: TextAlign.center,
-                //           ),
-                //         ],
-                //       );
-                //     },
-                //     indexedItemBuilder: (context, element, index) {
-                //       return ExpansionTile(
-                //         trailing: Icon(
-                //           Icons.arrow_drop_down_circle,
-                //           color: Colors.yellow.shade300,
-                //           size: 25,
-                //         ),
-                //         title: Text(
-                //           "More details",
-                //           style: TextStyle(
-                //             fontSize: 13.0,
-                //             color: Colors.yellow.shade300,
-                //             fontStyle: FontStyle.italic,
-                //             fontWeight: FontWeight.bold,
-                //           ),
-                //         ),
-                //         children: [Text(element.toString())],
-                //       );
-                //     },
-                //     itemBuilder:
-                //         (context, Map<String, List<dynamic>> element1) {
-                //       var league_team = element1['team'][0];
-                //       var colorindex =
-                //           distinct_leagues.toList().indexOf(league_team[0]);
-                //       if (colorindex > randomColors.length) {
-                //         randomColors = randomColors..addAll(randomColors);
-                //       }
-
-                //       Color1 = (randomColors)[0];
-
-                //       return
-                //
-                // Column(
-                //         children: [
-                //           Text(
-                //             league_team,
-                //             style: globals.noble,
-                //           ),
-                //           Container(
-                //             width: MediaQuery.of(context).size.width,
-                //             child: Card(
-                //               shape: RoundedRectangleBorder(
-                //                 borderRadius: BorderRadius.circular(20.0),
-                //               ),
-                //               child: Container(
-                //                 decoration: BoxDecoration(
-                //                     borderRadius: BorderRadius.circular(20.0),
-                //                     gradient: LinearGradient(
-                //                       begin: Alignment.bottomLeft,
-                //                       end: Alignment.topRight,
-                //                       colors: [
-                //                         Color1,
-                //                         Color1.withOpacity(0.6),
-                //                         // Color(0xff5983F3),
-                //                         // Color(0xff5983F3).withOpacity(0.6),
-                //                       ],
-                //                     )),
-                //                 padding: const EdgeInsets.all(8),
-                //                 child: Column(
-                //                   children: [
-                //                     Text(
-                //                       'Your Selection',
-                //                       style: globals.noble,
-                //                     ),
-                //                     for (var i
-                //                         in element1.keys.toList().sublist(1))
-                //                       Column(
-                //                         children: [
-                //                           Text(
-                //                             i.toString(),
-                //                             style: globals.noble,
-                //                           ),
-                //                           Text(element1[i].toString()
-                //                               // element1[i].toString() +
-                //                               // element1[i].toString() +
-                //                               // element1[i].toString() +
-                //                               // element1[i].toString(),
-                //                               ),
-                //                         ],
-                //                       ),
-                //                   ],
-                //                 ),
-                //               ),
-                //             ),
-                //           ),
-                //         ],
-                //       );
-                //     })
-
-                : CircularProgressIndicator()
-
-        // ListView.builder(
-        //     shrinkWrap: true,
-        //     itemCount: fantasydata.length,
-        //     itemBuilder: (context, index) {
-        //       return Card(
-        //         child: Padding(
-        //           padding: const EdgeInsets.all(8.0),
-        //           child: Column(
-        //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //             crossAxisAlignment: CrossAxisAlignment.start,
-        //             children: [
-        //               Text(fantasydata[index][0]),
-        //               Text(fantasydata[index][1]),
-        //               Text(fantasydata[index][2]),
-        //               Text(fantasydata[index][3]),
-        //             ],
-        //           ),
-        //         ),
-        //       );
-        //     }),
-
-        // Container(
-        //   child: Text(
-        //     fantasydata.toString(),
-        //     style: TextStyle(fontFamily: 'Cocosharp', color: Colors.black87),
-        //   ),
-        // ),
-        );
+                : CircularProgressIndicator());
   }
 }
